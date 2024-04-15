6fe74023867bbdac68a7a34fa7fd0957bf098c27
linpack12 <55255203+linpack12@users.noreply.github.com>
2024-04-15 11:21:20 +0200

    Initial setup

---
+++ Source/SPMPhysics/SPMPhysics.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SPMPhysics.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SPMPhysics, "SPMPhysics" );
